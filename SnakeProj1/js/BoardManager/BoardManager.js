784086de25adab16ff01204904f54a71974f1e70
Shane <shanegoodson101@gmail.com>
2014-04-03 15:30:22 +0300

    Externalised html templates
    Implementation code still needs to be fixed

--- SnakeProj1/js/BoardManager/BoardManager.js
+++ SnakeProj1/js/BoardManager/BoardManager.js
@@ -1,45 +1,51 @@
 /**
  * Created by Shane on 18/03/2014.
  */
-var BoardManager = new function() {
-
-    this.testGetBoardData = function(){
-      return boardNodesData;
+var BoardManager = new function () {
+    var rowMarkup = "";
+    var nodeMarkup = "";
+    this.testGetBoardData = function () {
+        return boardNodesData;
     };
 
     var boardNodesData = [];
 
-    this.populate = function(){
-        var rowMarkup = "<div id='row-${topCoord}' class='rowClass' style='top: ${top}'></div>";
-        var nodeMarkup = "<div id='node-${topCoord}-${leftCoord}' class='nodeClass' style='left: ${left}'></div>";
+    this.populate = function () {
 
-        $.template("rowTemplate", rowMarkup);
-        $.template("nodeTemplate", nodeMarkup);
-
-        for(var i=0; i<constants.gridHeight; i++){
-            boardNodesData[i]={}
-            var $row = $.tmpl("rowTemplate", {topCoord: (i*10), top: (i*10).toString()+"px"});
-            $row.appendTo('#gameBoard');
-
-            for(var j = 0; j< constants.gridWidth; j++){
-                var $node = $.tmpl("nodeTemplate", {topCoord:(i*10),leftCoord: (j*10), left: (j*10).toString()+"px"});
-                boardNodesData[i][j] = {$element: $node, ClassManager: ClassManager.create($node, {class: 'nodeClass'})};
-                $("#row-"+(i*10)).append($node);
-
-
-
-
+//Fix this -- shoddy code
+        $.get('js/BoardManager/RowTemplate.html', function (data) {
+                rowMarkup = data.toString();
+                $.get('js/BoardManager/NodeTemplate.html', function (data) {
+                        nodeMarkup = data.toString();
+                        populateBoard();
+                    }
+                );
             }
-        }
+        );
     };
 
-   this.setClassToNode = function(position,className){
+
+    function populateBoard() {
+        for (var i = 0; i < constants.gridHeight; i++) {
+            boardNodesData[i] = {};
+            var $row = $.tmpl(rowMarkup, {topCoord: (i * 10), top: (i * 10).toString() + "px"});
+            $row.appendTo('#gameBoard');
+
+            for (var j = 0; j < constants.gridWidth; j++) {
+                var $node = $.tmpl(nodeMarkup, {topCoord: (i * 10), leftCoord: (j * 10), left: (j * 10).toString() + "px"});
+                boardNodesData[i][j] = {$element: $node, ClassManager: ClassManager.create($node, {class: 'nodeClass'})};
+                $("#row-" + (i * 10)).append($node);
+            }
+        }
+    }
+
+    this.setClassToNode = function (position, className) {
         boardNodesData[position.top][position.left].ClassManager.setClass(className);
     };
 
-   this.resetNode = function(position){
+    this.resetNode = function (position) {
         boardNodesData[position.top][position.left].ClassManager.reset();
-   };
+    };
 
 };
 
